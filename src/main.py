34ba78b3deb2857d8f639ac191cdf4819345d42a
t-nician <dev@senpai.zone>
2024-05-17 19:21:14 -0400

    It'll be worth it bro!

--- src/main.py
+++ src/main.py
@@ -1,23 +1,3 @@
-import json
 import rconutil
 
 
-host, port, password = "", -1, ""
-
-
-with open("creds.json", "r") as file:
-    content = file.read()
-    data = json.loads(content)
-
-    host = data["host"]
-    port = data["port"]
-    password = data["password"]
-
-
-client = rconutil.client.RconClient(
-    host=host,
-    port=port,
-    password=password
-)
-
-client.connect()
