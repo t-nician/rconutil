09d1a1a17145fae1afca541fe1d31e7589d8c3e9
t-nician <dev@senpai.zone>
2024-05-24 19:59:58 -0400

    Okay, for now I'm going to now work on creating my core.server section.

---
+++ src/rconutil/secure/packet.py
@@ -0,0 +1,9 @@
+from rconutil.core.packet import RconPacket, SendPacketType, ReceivePacketType
+
+from enum import Enum
+from dataclasses import dataclass, field
+
+
+@dataclass
+class SecureRconPacket:
+    pass
---
+++ src/rconutil/secure/server.py
@@ -0,0 +1,9 @@
+from rconutil.secure import packet
+
+
+from socket import socket, AF_INET, SOCK_STREAM
+from dataclasses import dataclass, field
+
+
+class SecureRconServer:
+    pass
